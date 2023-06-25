20d20710cff550a6180ed4d2a170e66b12855caf
brigazvi <37640970+brigazvi@users.noreply.github.com>
2023-06-25 15:21:43 +0300

    installing three.js + ReactThreeFiber and initial folders for the 3d models

---
+++ src/Components/Viewer3d/index.jsx
@@ -0,0 +1,8 @@
+function Viewer3d() {
+  return (
+    <Canvas>
+      <Closet4Doors />
+    </Canvas>
+  )
+}
+export default Viewer3d
---
+++ src/Test/Tzvi.jsx
@@ -0,0 +1,8 @@
+import { Closet4Doors } from "../Components/3dModels/Closet4Doors"
+
+function Tzvi() {
+    return (
+
+    )
+}
+export default Tzvi
