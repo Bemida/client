fe8a231a270232e83b408920cc5620c20909bd2e
Yehuda Yeger <yeger921@gmail.com>
2023-06-25 18:11:26 +0300

    added both order and item fake data

--- src/Data/fakeData.js
+++ src/Data/fakeData.js
@@ -1,52 +1,33 @@
 //Fake data 
-const fakeOrderData = ({
-    user: {
-        type: String,
-        required: false // for the moment
-    },
-    status : {
-        type : String,
-        default : "in_progress",
-        enum : ["in_progress","confirmed"]
-    },
-    date : {
-        type : Date,
-        default : Date.now
-    },
-    shipping : {
-        type : String,
-        default : "regular",
-        enum : ["regular","express"]
-    },
-    stages : [{
-        stage : [{
-            stageNo : {
-                type : Number,
-                default : 1 ,
-                enum : [1,2,3,4]
-            },
-            furniture : {
-                type : String,
-                enum : ["Closet","Bed","Bookcase","Table","Console","Drawers"],
-                default : "Closet"
-            },
-            items : [{
+const fakeOrderData = {
+    user: "John Doe",
+    status: "in_progress",
+    date: "2023-06-25T12:34:56.789Z",
+    shipping: "regular",
+    stages: [
+      {
+        stage: [
+          {
+            stageNo: 1,
+            furniture: "Closet",
+            items: [
+              {
                 item: {
-                    name : String,
-                    itemId : String,
-                    amount : Number,
-                    img : "url",
-                    price : Number
+                  name: "Table",
+                  itemId: "67890",
+                  amount: 1,
+                  img: "🪑",
+                  price: 100
                 }
-            }]
-        }]
-    }],
-    total: {
-        type : Number,
-        default : 0
-    }
-})
-
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    total: 150
+  }
+  
 
 
 const fakeItemData = {
