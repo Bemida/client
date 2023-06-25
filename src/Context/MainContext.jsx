cb5f1b47465f410cba7091897c561cea30d33810
Yehuda Yeger <yeger921@gmail.com>
2023-06-25 19:44:23 +0300

    updated context

--- src/Context/MainContext.jsx
+++ src/Context/MainContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react"
-import { items } from '../Data/fakeData'
+import { items,orders } from '../Data/fakeData'
 
 //This is where we'll manager any context that needs to be used across our applications. 
 //Below we've created a component that contains multiple states. We can later wrap a component with this one to give it access to all our different contexts
@@ -9,8 +9,9 @@ const DataContext = createContext({})
 export const ContextProvider = ({ children }) => {
     const [stage, setStage] = useState({})
 
+
     return (
-        <DataContext.Provider value={{ stage, setStage, fakeData: { items } }}>
+        <DataContext.Provider value={{ stage, setStage, fakeData: { items,orders } }}>
             {children}
         </DataContext.Provider>)
 
