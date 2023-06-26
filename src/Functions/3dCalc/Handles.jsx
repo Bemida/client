0a9655922c3952815f1393f77f92b4f4162a2d65
David <computerdavid494@gmail.com>
2023-06-26 13:10:11 +0300

    f-handels

--- src/Functions/3dCalc/Handles.jsx
+++ src/Functions/3dCalc/Handles.jsx
@@ -6,21 +6,25 @@ export default function Handles(width, height, depth, doorsNumber, woodWidth = 1
     const positionalRatio = 2; // Between door and handle
     let isLeftDoor = 1; // is left door
     const doorWidth = width / doorsNumber;
-    const lastXposition = woodWidth; // start from (0, 0, 0)
+    let lastXposition = woodWidth; // start from (0, 0, 0)
     const xPosition = isLeftDoor ? doorWidth - positionalRatio : width - doorWidth + positionalRatio;
     const yPosition = height > 135 ? 130 : height - 5;
-    const handels = Array.from({ length: doorsNumber }, (_, i) => i + 1).map(number => {
-        handels.push(
-            < mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.handle2.geometry}
-                material={materials.chrome}
-                position={[xPosition, yPosition, depth]}
-                rotation={[0, -1.571, 0]}
-            />)
-        isLeftDoor = number % 2 === 0;
-        lastXposition += doorWidth;
+    const handels = Array.from({ length: doorsNumber }, (_, i) => i + 1);
+    handels.map(number => {
+        return (
+            <>
+                < mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.handle2.geometry}
+                    material={materials.chrome}
+                    position={[xPosition, yPosition, depth]}
+                    rotation={[0, -1.571, 0]}
+                />
+                {isLeftDoor = number % 2 === 0}
+                {lastXposition += doorWidth}
+            </>
+        )
     })
 
     return handels;
