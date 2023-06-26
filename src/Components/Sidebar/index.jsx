fe29d0992b32b713213799ab00d25d566601e912
j s <grnwtmry@gmail.com>
2023-06-26 10:50:42 +0300

    comments

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,15 +1,39 @@
-import styles from "./style.module.css"
-import data from "../../Data/fakeData"
+import styles from "./style.module.css";
+import data from "../../Data/fakeData";
+import { useEffect, useState } from "react";
 function SideBar() {
-    const newData = [...data.items.styling[0].interior];
-    console.log(newData);
+  const tabs = Object.entries(data.items.exterior);
+  const [openTab, setOpenTab] = useState();
+  // useEffect(() => {
+
+  // },[openTab])
+  console.log(tabs);
+  const handleClick = (tab) => {
+    setOpenTab(tab);
+  };
   return (
     <div className={styles.outerContainer}>
-    <div className={styles.sidebarContainer}>
+      <div className={styles.sidebarContainer}>
+        {tabs.map((tab, i) => {
+   if(openTab){
+            if(tab[0] === openTab) {
+              return (<><ul onClick={() => handleClick(tab[0])} key={i}>{tab[0]}</ul>
+                  {tab[1].map((tab) => {
+                    return <li>{tab.description}</li>;
+                  })}
+                </>
+              );
+            }
+        }
+           return (
+            <ul key={i} onClick={() => handleClick(tab[0])}>
+              {tab[0]}
+            </ul>
+       );
+        })}
+      </div>
     </div>
-
-    </div>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
