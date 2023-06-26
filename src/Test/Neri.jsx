66c43df2cd1d75c00b539f7c65dc3ff3f1cf2201
neri <neri9931@gmail.com>
2023-06-26 16:58:50 +0300

    progress

--- src/Test/Neri.jsx
+++ src/Test/Neri.jsx
@@ -1,7 +1,8 @@
 import ProgressBar from "../Components/ProgressBar";
+import Header from "../Layout/Header";
 
 function Neri() {
-  return <ProgressBar />;
+  return <Header />;
 }
 
 export default Neri;
