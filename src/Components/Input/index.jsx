15ca82a611cf77e4e2b24ab151efef7aaf850f93
haya <aminovhaya@gmail.com>
2023-06-26 12:10:06 +0300

    input validation: red border if data is invalid

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -1,23 +1,21 @@
 import styles from './style.module.css'
 
-// props : label, id, type:text/number, isRequired
+// props : label, id, type:text/number, isRequired, isValid
 // creator:Haya Aminov - 
+//validation:isRequired+ red border if data is not valid
 
-//validation/isRequired
 function Input(props) {
-    //debugger;
-    console.log(props.isRequired)
-    const handleChange = (e) => {
-        debugger;
-        console.log(e.target.value)
-    }
-    return (
-        // <div className={styles.name} style={style} {...props}>
-        <div>
-            <label htmlFor={props.id}>{props.label}</label>
-            {props.isRequired ? <input id={props.id} type={props.type} required onChange={handleChange} min={10} max={20} /> : <input type={props.type} id={props.id} onChange={handleChange} min={10} max={20} />}
 
-            {/* <input id={props.id} className='red' type={props.type} required /> */}
+
+    return (
+
+
+        <div className={styles.name} style={props.style || {}}  {...props}>
+            <label className={styles.label} htmlFor={props.id}>{props.label}</label>
+            {props.isRequired ?
+                <input className={`${styles.input} ${props.isValid ? '' : styles.invalid}`} id={props.id} type={props.type} required />
+                : <input className={`${styles.input} ${props.isValid ? '' : styles.invalid}`} type={props.type} id={props.id} />}
+
         </div>
     )
 }
--- src/Test/Haya.jsx
+++ src/Test/Haya.jsx
@@ -5,12 +5,12 @@ export default function Haya() {
     console.log(numberDoors(65))
     return (
         <div>
-            <form action=''>
-                <Input type='number' isRequired={true} label='width in cm' id='width' />
-                <Input type='number' isRequired={true} id='depth' />
-                <Input type='number' label='height in cm' id='height' />
-                <input type="submit" value="Submit" />
-            </form>
+            {/* <form action=''> */}
+            <Input type='number' isRequired={true} label='width in cm' id='width' isValid={false} />
+            <Input type='number' isRequired={true} id='depth' isValid={true} />
+            <Input type='number' label='height in cm' id='height' isValid={false} />
+            <input type="submit" value="Submit" />
+            {/* </form> */}
         </div>
     )
 }
