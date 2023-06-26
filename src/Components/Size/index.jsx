bf46b58ba7f3215b7468276498f5c71a6e503d91
unknown <dvd3418@gmail.com>
2023-06-26 16:00:58 +0300

    aaaaaaaaaaa

--- src/Components/Size/index.jsx
+++ src/Components/Size/index.jsx
@@ -1,13 +1,13 @@
+import { Input } from 'postcss'
 import styles from './style.module.css'
 
 //Creator : didi -create component
-
 
 function Size() {
 
   return (
     <div className={styles.containerSize}>
-      
+      <Input />
     </div>
   )
 }
