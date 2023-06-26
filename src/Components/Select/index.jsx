1b8f09c5f60ad256ee5b8f21d09676769f8f6a27
neri <neri9931@gmail.com>
2023-06-26 11:16:50 +0300

    end selectAndHeader

--- src/Components/Select/index.jsx
+++ src/Components/Select/index.jsx
@@ -1,16 +1,35 @@
 import styles from "./style.module.css";
 
-// props: { {style} , options: [{key:value},{key:value}...] }
+// props: { style({}) , options: [{key:"placeholder"},{key:"",value:""},{key:"",value:""}...] }
 // creator: Neri
 
-function Select({ style, options, name, onChange }) {
+function Select({ options, style, name, onChange }) {
   return (
-    <select style={style} className={styles.select}>
-      {options.map((op) => (
-        <option value={op.value} className={styles.option}>
-          {op.key}
-        </option>
-      ))}
+    <select
+      name={name}
+      style={style}
+      className={styles.select}
+      onChange={onChange}
+    >
+      {options.map((op, i) => {
+        if (i === 0) {
+          return (
+            <option
+              hidden
+              value={op.value}
+              className={styles.option}
+              key={op.value}
+            >
+              {op.key}
+            </option>
+          );
+        }
+        return (
+          <option value={op.value} className={styles.option} key={op.value}>
+            {op.key}
+          </option>
+        );
+      })}
     </select>
   );
 }
