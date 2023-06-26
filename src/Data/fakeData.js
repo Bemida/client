8f90d4046ee90707a86341ea3bdbe6a20a0d2d0e
yosefus <yosalsoni@gmail.com>
2023-06-26 16:02:46 +0300

    fix ידיות,

--- src/Data/fakeData.js
+++ src/Data/fakeData.js
@@ -3,68 +3,69 @@
 
 
 const fakeItemData = {
-    categories: [
-        { id: 21432, category: "ארון בגדים" },
-        { id: 456, category: "מיטה" },
-        { id: 7893, category: "ספריה" },
-        { id: 2768, category: "שולחן" },
-        { id: 435, category: "קונסולה" },
-        { id: 123, category: "שידות מגירות" }],
-    exterior: {
+  categories: [
+    { id: 21432, category: "ארון בגדים" },
+    { id: 456, category: "מיטה" },
+    { id: 7893, category: "ספריה" },
+    { id: 2768, category: "שולחן" },
+    { id: 435, category: "קונסולה" },
+    { id: 123, category: "שידות מגירות" }],
+  exterior: {
 
-        handles: {
-            name: ידיות,
-            list: [{
-                description: "ידית כפתור נרקיס",
-                img: "🎭",
-                id: 321
-            }, {
-                description: "ידית זהב מוברש",
-                img: "🥎",
-                id: 54
-            }
-            ]
-        },
-        base: {
-
-            name: "בסיס",
-            list: [{
-
-                description: "רגליים",
-                img: "🦵",
-                id: 283
-            }, {
-                description: "צוקל",
-                img: "🧆",
-                id: 12
-            }]
-        },
-        color: {
-            name: "צבע",
-            list: [{
-                description: "אורן",
-                img: "🎋",
-                id: 45
-            }, {
-                description: "אלון",
-                img: "🎄",
-                id: 12
-            }, {
-                description: "לבן",
-                img: "🥚",
-                id: 54
-            }]
-        }
+    handles: {
+      name: "ידיות",
+      list: [{
+        description: "ידית כפתור נרקיס",
+        img: "🎭",
+        id: 321
+      }, {
+        description: "ידית זהב מוברש",
+        img: "🥎",
+        id: 54
+      }
+      ]
     },
+    base: {
 
-interior:{
-   name: פנימי,
-   list: {
-        מדפים: 1,
-        מגירות: 0,
-        מוט: true
+      name: "בסיס",
+      list: [{
+
+        description: "רגליים",
+        img: "🦵",
+        id: 283
+      }, {
+        description: "צוקל",
+        img: "🧆",
+        id: 12
+      }]
+    },
+    color: {
+      name: "צבע",
+      list: [{
+        description: "אורן",
+        img: "🎋",
+        id: 45
+      }, {
+        description: "אלון",
+        img: "🎄",
+        id: 12
+      }, {
+        description: "לבן",
+        img: "🥚",
+        id: 54
+      }]
     }
-}}
+  },
+
+  interior: {
+    name: "פנימי",
+    list: {
+      מדפים: 1,
+      מגירות: 0,
+      מוט: true
+    }
+  }
+}
 
 const fakeOrderData = {
   user: "John Doe",
@@ -117,12 +118,12 @@ const order = {
   readyToNext: false,
   order: {
 
-    }
+  }
 }
 
 export default {
-    items: fakeItemData,
-    orders: fakeOrderData,
-    order
+  items: fakeItemData,
+  orders: fakeOrderData,
+  order
 }
 
