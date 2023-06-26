5b6ef01122d4802cb8f200c0ad65502b1602b278
j s <grnwtmry@gmail.com>
2023-06-26 08:37:04 +0300

    first sidebar structure

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import styles from "./style.module.css"
+import data from "../../Data/fakeData"
 function SideBar() {
+    const newData = [...data.items.styling[0].interior];
+    console.log(newData);
   return (
-    <div>
-      nnnn
+    <div className={styles.outerContainer}>
+    <div className={styles.sidebarContainer}>
+    </div>
+
     </div>
   )
 }
