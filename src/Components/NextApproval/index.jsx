087e6827abb5c0fab366dc5388ef28124dbd0f1d
neri <neri9931@gmail.com>
2023-06-27 11:04:32 +0300

    payment

---
+++ src/Components/NextApproval/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./style.module.css";
+
+// props: {}
+// creator: Neri
+
+function NextApproval() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default NextApproval;
