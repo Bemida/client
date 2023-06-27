1cfa4a92bda0e9a880beae6e2ba760163ae82553
Yehuda Yeger <yeger921@gmail.com>
2023-06-27 10:26:54 +0300

    added api instructions

--- src/Test/Neri.jsx
+++ src/Test/Neri.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { DataContext } from '../Context/MainContext'
 
 export default function Neri() {
+
+
+  const context = useContext(DataContext)
+  
   return (
     <div>Neri</div>
   )
