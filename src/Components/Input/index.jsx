4dae56e736b4720fe7b07fcdb17dc51216a3bed3
haya <aminovhaya@gmail.com>
2023-06-27 10:33:16 +0300

    branch haya- input validation and update context

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -1,11 +1,11 @@
 import styles from './style.module.css'
-
-// props : label, id, type:text/number, isRequired, isValid
+import { DataContext } from '../../Context/MainContext'
+import { useContext } from 'react'
+// props : label, id, type:text/number, isRequired, isValid, onChange, min, max
 // creator:Haya Aminov - 
-//validation:isRequired+ red border if data is not valid
+//validation:isRequired & red border if data is not valid
 
 function Input(props) {
-
 
     return (
 
@@ -13,8 +13,8 @@ function Input(props) {
         <div className={styles.name} style={props.style || {}}  {...props}>
             <label className={styles.label} htmlFor={props.id}>{props.label}</label>
             {props.isRequired ?
-                <input className={`${styles.input} ${props.isValid ? '' : styles.invalid}`} id={props.id} type={props.type} required />
-                : <input className={`${styles.input} ${props.isValid ? '' : styles.invalid}`} type={props.type} id={props.id} />}
+                <input className={`${styles.input} ${props.isValid ? '' : styles.invalid}`} id={props.id} type={props.type} onChange={props.onChange} min={props.min} max={props.max} required />
+                : <input className={`${styles.input} ${props.isValid ? '' : styles.invalid}`} type={props.type} id={props.id} onChange={props.onChange} min={props.min} max={props.max} />}
 
         </div>
     )
--- src/Test/Haya.jsx
+++ src/Test/Haya.jsx
@@ -2,17 +2,20 @@ import Input from '../Components/Input'
 import numberDoors from '../Functions/numberDoors'
 import Button from '../Components/Button'
 import { useContext } from 'react';
-//import MainContext from '../Context/MainContext
 import { DataContext } from '../Context/MainContext'
-
+import { useState } from 'react';
 
 export default function Haya() {
-    const context = useContext(DataContext)
-    // console.log(context);
-    // console.log(context.fakeData.items.categories)
+    const context = useContext(DataContext);
 
-    //console.log(x)
-    //console.log(numberDoors(65))
+    const [widthValue, setWidthValue] = useState();
+    const [heightValue, setHeightValue] = useState();
+    const [depthValue, setDepthValue] = useState();
+
+    const extremeWidth = { min: 100, max: 150 };
+    const extremeDepth = { min: 20, max: 60 };
+    const extremeHeight = { min: 120, max: 180 };
+
     const selection = [{
         width: 60,
         height: 120,
@@ -22,22 +25,51 @@ export default function Haya() {
         color: 'beige',
         material: 'wood'
     }
-    ]
-    function handleOnClick() {
-        //  debugger;
-        // console.log('handle click')
+    ];
+
+
+
+    function handleOnClick() { //for click on next button==> updates the state
         console.log(context.order)
-        context.setOrder({ stageNo: context.order.stageNo + 1, order: { ...context.order.order, ...selection[context.order.stageNo - 1] } })
-        // console.log(context.order);
+        context.setOrder({ stageNo: context.order.stageNo, order: { ...context.order.order, ...selection[context.order.stageNo - 1] } })
+    }
+
+    function updateState(elmt) {
+        switch (elmt.id) {
+            case 'width':
+                setWidthValue(Number(elmt.value));
+                break;
+            case 'height':
+                setHeightValue(Number(elmt.value));
+                break;
+            case 'depth':
+                setDepthValue(Number(elmt.value));
+        }
+        console.log('width' + widthValue);
+        console.log('depth' + depthValue);
+        console.log('height' + heightValue);
+    }
+
+    function handleChange(e) {
+        updateState(e.target);
+        context.setOrder({ stageNo: context.order.stageNo, order: { ...context.order.order, [e.target.id]: Number(e.target.value) } })
+        console.log(widthValue);
+        console.log(depthValue);
+        console.log(heightValue);
+    }
+
+    function checkValue(value, min, max) {
+        let x = !value || value >= min && value <= max;
+        return x;
     }
     return (
         <div>
-            < Button onClick={handleOnClick} text='next' color='red' width='100px' height='30px' />
+            {/* < Button onClick={handleOnClick} text='next' color='red' width='100px' height='30px' /> */}
             {/* <form action=''> */}
-            {/* <Input type='number' isRequired={true} label='width in cm' id='width' isValid={false} />
-            <Input type='number' isRequired={true} id='depth' isValid={true} />
-            <Input type='number' label='height in cm' id='height' isValid={false} />
-            <input type="submit" value="Submit" /> */}
+            <Input type='number' isRequired={true} label='width in cm' id='width' isValid={checkValue(widthValue, extremeWidth.min, extremeWidth.max)} min={100} max={150} onChange={handleChange} />
+            <Input type='number' isRequired={true} id='depth' isValid={checkValue(depthValue, extremeDepth.min, extremeDepth.max)} min={120} max={180} onChange={handleChange} />
+            <Input type='number' label='height in cm' id='height' isValid={checkValue(heightValue, extremeHeight.min, extremeHeight.max)} min={20} max={60} onChange={handleChange} />
+            {/* <input type="submit" value="Submit" />  */}
             {/* </form> */}
         </div>
     )
