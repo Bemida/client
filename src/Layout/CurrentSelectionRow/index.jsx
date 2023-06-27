b72358e303da35bed7a52b6f1c4682bf2f12286e
marlan101 <arlanm@gmail>
2023-06-27 15:24:38 +0300

    coomit

--- src/Layout/CurrentSelectionRow/index.jsx
+++ src/Layout/CurrentSelectionRow/index.jsx
@@ -1,1 +1,17 @@
-export default Template
+import styles from './style.module.css'
+
+// props: key, value 
+// creator: Michael Arlan
+//
+function CurrentSelectionRow({...props} ) {
+
+  return (
+    <div className={styles.name} style={style} {...props}>
+
+        CurrentSelectionRow File 
+        
+    </div>
+  )
+}
+
+export default CurrentSelectionRow
