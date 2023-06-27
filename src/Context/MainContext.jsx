a035bff34b0885ab7ae75bb69b59983e1081f5df
Yehuda Yeger <yeger921@gmail.com>
2023-06-27 15:41:03 +0300

    updated context

--- src/Context/MainContext.jsx
+++ src/Context/MainContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react"
 import fakeData from "../Data/fakeData"
 import data from '../Data/fakeData'
-
+import newFakeData from '../Data/fakeDataTest'
 
 //This is where we'll manager any context that needs to be used across our applications. 
 //Below we've created a component that contains multiple states. We can later wrap a component with this one to give it access to all our different contexts
@@ -11,9 +11,11 @@ export const DataContext = createContext({})
 const ContextProvider = ({ children }) => {
     // const [stage, setStage] = useState({})
     const [order, setOrder] = useState(data.order)
+    const [fullOrder,setFullOrder] = useState(newData.emptyOrderData)
+    const [readyToNext,setReadyToNext] = useState(true)
 
     return (
-        <DataContext.Provider value={{ fakeData: data, order,setOrder }}>
+        <DataContext.Provider value={{ fakeData: data, order,setOrder, newFakeData }}>
             {children}
 
         </DataContext.Provider>)
---
+++ src/Data/fakeDataTest.js
@@ -0,0 +1,179 @@
+//Fake data
+
+const fakeItemData = {
+    categories: [
+        { id: 21432, category: "ארון בגדים", isAvailable: true },
+        { id: 456, category: "מיטה", isAvailable: false },
+        { id: 7893, category: "ספריה", isAvailable: false },
+        { id: 2768, category: "שולחן", isAvailable: false },
+        { id: 435, category: "קונסולה", isAvailable: false },
+        { id: 123, category: "שידות מגירות", isAvailable: false },
+    ],
+    exterior: {
+        handles: {
+            name: "ידיות",
+            key: "handleId",
+            list: [
+                {
+                    description: "ידית כפתור נרקיס",
+                    img: "🎭",
+                    id: 321,
+                },
+                {
+                    description: "ידית זהב מוברש",
+                    img: "🥎",
+                    id: 54,
+                },
+            ],
+        },
+        base: {
+            name: "בסיס",
+            key: "base",
+            list: [
+                {
+                    description: "רגליים",
+                    img: "🦵",
+                    id: 283,
+                },
+                {
+                    description: "צוקל",
+                    img: "🧆",
+                    id: 12,
+                },
+            ],
+        },
+        color: {
+            name: "צבע",
+            key: "color",
+            list: [
+                {
+                    description: "אורן",
+                    img: "🎋",
+                    id: 45,
+                },
+                {
+                    description: "אלון",
+                    img: "🎄",
+                    id: 12,
+                },
+                {
+                    description: "לבן",
+                    img: "🥚",
+                    id: 54,
+                },
+            ],
+        },
+    },
+
+    interior: {
+        name: "פנימי",
+        section1: {
+            name: "עמודה ימין",
+            key: "section1",
+            list: {
+                shelves: {
+                    name: "מדפים",
+                    key: "shelves",
+                    type: "number"
+                },
+                hangingRod: {
+                    name: "מוט תלייה",
+                    key: "hangingRod",
+                    type: "boolean"
+                }
+            }
+        },
+        section2: {
+            name: "עמודה שמאל",
+            key: "section2",
+            list: {
+                shelves: {
+                    name: "מדפים",
+                    key: "shelves",
+                    type: "number"
+                },
+                hangingRod: {
+                    name: "מוט תלייה",
+                    key: "hangingRod",
+                    type: "boolean"
+                }
+            }
+        }
+    },
+};
+
+const fakeOrderData = {
+
+    userName: "John Doe",
+    userPhone: "082345678",
+    userEmail: "test@test.com",
+
+    status: "in_progress",
+    shipping: "regular",
+    furniture: "Closet",
+    color: "red",
+    section1: {
+        shelves: 3,
+        hangingRod: true,
+        drawers: 4,
+    },
+    section2: {
+        shelves: 2,
+        hangingRod: true,
+        drawers: 2,
+    },
+    material: "wood",
+    height: 200,
+    width: 100,
+    depth: 50,
+    doors: 2,
+    color: "brown",
+    handleId: "5678",
+    stageNo: 5,
+    base: "legs",
+    total: 1000,
+};
+
+ const emptyFakeOrderData = {
+
+    userName: null,
+    userPhone: null,
+    userEmail: null,
+
+    status: null,
+    shipping: null,
+    furniture: null,
+    color: null,
+    section1: {
+        shelves: 0,
+        hangingRod: true,
+        drawers: 0,
+    },
+    section2: {
+        shelves: 0,
+        hangingRod: true,
+        drawers: 0,
+    },
+    material: null,
+    height: 0,
+    width: 0,
+    depth: 0,
+    doors: 0,
+    color: null,
+    handleId: null,
+    stageNo: 1,
+    base: null,
+    total: 0,
+};
+
+const order = {
+    readyToNext: false,
+    order: {},
+};
+
+export default {
+    items: fakeItemData,
+    orders: fakeOrderData,
+    order,
+    emptyOrderData : emptyFakeOrderData,
+};
