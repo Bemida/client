5660d6458996f505cbf3871d48aec4bc579fb7b8
neri <neri9931@gmail.com>
2023-06-28 12:51:10 +0300

    after marge

--- src/Functions/submitOfNext.js
+++ src/Functions/submitOfNext.js
@@ -1,64 +1,57 @@
-function validationList(stageNow) {
-  switch (stageNow) {
-    // value: null or function
-    // function : if error - do throw
-    case 1:
-      return {
-        furniture: null,
-        example: () => alert("not-good"),
-      };
-    case 2:
-      return {
-        height: null,
-        width: null,
-        depth: null,
-        doors: null,
-      };
-    case 3:
-      return {
-        handleId: null,
-        color: null,
-        base: null,
-      };
-    case 4:
-      return {
-        section1_drawers: null,
-        section1_hangingRod: null,
-        section1_shelves: null,
-        section2_drawers: null,
-        section2_hangingRod: null,
-        section2_shelves: null,
-      };
-    default:
-      return {};
-  }
-}
+let validation = {
+  // value: null or function
+  // function : if error - return "msg"
+
+  1: {
+    furniture: null,
+  },
+  2: {
+    height: null,
+    width: null,
+    depth: null,
+    doors: null,
+  },
+  3: {
+    handleId: null,
+    color: null,
+    base: null,
+  },
+  4: {
+    section1_drawers: null,
+    section1_hangingRod: null,
+    section1_shelves: null,
+    section2_drawers: null,
+    section2_hangingRod: null,
+    section2_shelves: null,
+  },
+};
 
 export default function submitOfNext(context) {
   console.log(context); //
   console.log("ordrer: " + context.fullOrder); //
-  try {
-    const validation = validationList(context.stage);
-    const keys = Object.keys(validation);
 
-    keys.forEach((key) => {
-      const value = validation[key];
-      if (!value) {
-        let valueFromContext = context.fullOrder[key];
-        if (key.includes("_")) {
-          const [key1, key2] = key.split("_");
-          valueFromContext = context.fullOrder[key1][key2];
-        }
+  const errorsList = [];
+  const keys = Object.keys(validation);
 
-        if (!valueFromContext) {
-          throw "MUST";
-        }
-      } else {
-        value();
+  validation = validation[context.stage]
+
+  keys.forEach((key) => {
+    const value = validation[key];
+    if (!value) {
+      let valueFromContext = context.fullOrder[key];
+      if (key.includes("_")) {
+        const [key1, key2] = key.split("_");
+        valueFromContext = context.fullOrder[key1][key2];
       }
-    });
-    return true;
-  } catch (err) {
-    console.log(err); //
-  }
+
+      if (!valueFromContext) {
+        errorsList.push({ key, msg: "--Must be entered!--" });
+      }
+    } else if (typeof valueFromContext === "function") {
+      let msg = value();
+      if (msg) push({ key, msg });
+    } else {
+    }
+  });
+  return errorsList;
 }
