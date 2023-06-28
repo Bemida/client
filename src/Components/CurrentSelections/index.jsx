871cf4da237882f6222f09fdf04821a9f53d8f4e
marlan101 <arlanm@gmail>
2023-06-28 12:42:30 +0300

    Initial Display

---
+++ src/Components/CurrentSelections/index.jsx
@@ -0,0 +1,63 @@
+import SelectionRow from '../../Layout/SelectionRow';
+import styles from './style.module.css'
+
+export const LanguageTranslater = {
+
+  userName: {heb: "שם", ordinal: 10, postfix: ''},// "John Doe",
+  userPhone: {heb: "טלפון", ordinal: 10, postfix: ''},// "082345678",
+  userEmail: {heb: 'דוא"ל'},// "test@test.com",
+
+  status: {heb: "סטטוס", ordinal: 10, postfix: ''},// "in_progress",
+  shipping: {heb: "שיטת משלוח", ordinal: 10, postfix: ''},// "regular",
+  furniture: {heb: "סוג", ordinal: 10, postfix: ''},// "Closet",
+  color: {heb: "צבע", ordinal: 10, postfix: ''},// "red",
+  section1: {
+      shelves: 3,
+      hangingRod: true,
+      drawers: 4,
+  },
+  section2: {
+      shelves: 2,
+      hangingRod: true,
+      drawers: 2,
+  },
+  material: {heb: "חומר", ordinal: 10, postfix: ''},// "wood",
+  height: {heb: "גובה", ordinal: 1, postfix: 'ס"מ'},// 200,
+  width: {heb: "רוחב", ordinal: 2, postfix: 'ס"מ'},// 100,
+  depth: {heb: "עומק", ordinal: 3, postfix: 'ס"מ'},// 50,
+  doors: {heb: "מספר דלתות", ordinal: 10, postfix: ''},// 2,
+  // color: {heb: "צבע", ordinal: 10, postfix: ''},// "brown",
+  handleId: {heb: "דגם ידית", ordinal: 10, postfix: ''},// "5678",
+  stageNo: {heb: "סוג בסיס", ordinal: 10, postfix: ''},// 5,
+  base: {heb: "", ordinal: 10, postfix: ''},// "legs",
+  // total: 1000,
+};
+
+
+// props : 
+// creator:
+function CurrentSelections({style = {} , selections} ) {
+  const keys = Object.keys(selections)
+  return (
+    <div className={styles.name} style={style} >
+
+      <ul className={styles.Selectionlist}>
+        {/* TODO: add sort() after filter */}
+        {keys
+        .filter((v)=>LanguageTranslater[v])
+        .sort((a,b)=>Number(LanguageTranslater[a]['ordinal'])<Number(LanguageTranslater[b]['ordinal'])?1:-1)
+        .map((k) => 
+          <li>{
+            typeof selections[k] === 'object' ? k : 
+            <SelectionRow bolder= {LanguageTranslater[k]['heb']} regular={selections[k] + ' ' + LanguageTranslater[k]['postfix']}/>
+
+            }
+          </li>)}
+
+      </ul>
+
+    </div>
+  )
+}
+
+export default CurrentSelections
--- src/Layout/DisplayCurrentSelection/index.jsx
+++ src/Layout/DisplayCurrentSelection/index.jsx
@@ -7,44 +7,11 @@ import { useContext } from 'react'
 
 import data  from '../../Data/fakeDataTest' //.orders
 import SelectionRow from '../SelectionRow'
+import CurrentSelections from '../../Components/CurrentSelections'
 
 
 // props : 
 // creator: Michael Arlan
-
-  const LanguageTranslater = {
-
-    // userName: {heb: "שם", ordinal: 10, postfix: ''},// "John Doe",
-    // userPhone: {heb: "טלפון", ordinal: 10, postfix: ''},// "082345678",
-    // userEmail: {heb: 'דוא"ל'},// "test@test.com",
-
-    // status: {heb: "סטטוס", ordinal: 10, postfix: ''},// "in_progress",
-    // shipping: {heb: "שיטת משלוח", ordinal: 10, postfix: ''},// "regular",
-    furniture: {heb: "סוג", ordinal: 10, postfix: ''},// "Closet",
-    color: {heb: "צבע", ordinal: 10, postfix: ''},// "red",
-    section1: {
-        shelves: 3,
-        hangingRod: true,
-        drawers: 4,
-    },
-    section2: {
-        shelves: 2,
-        hangingRod: true,
-        drawers: 2,
-    },
-    material: {heb: "חומר", ordinal: 10, postfix: ''},// "wood",
-    height: {heb: "גובה", ordinal: 1, postfix: 'ס"מ'},// 200,
-    width: {heb: "רוחב", ordinal: 2, postfix: 'ס"מ'},// 100,
-    depth: {heb: "עומק", ordinal: 3, postfix: 'ס"מ'},// 50,
-    doors: {heb: "מספר דלתות", ordinal: 10, postfix: ''},// 2,
-    // color: {heb: "צבע", ordinal: 10, postfix: ''},// "brown",
-    handleId: {heb: "דגם ידית", ordinal: 10, postfix: ''},// "5678",
-    stageNo: {heb: "סוג בסיס", ordinal: 10, postfix: ''},// 5,
-    base: {heb: "", ordinal: 10, postfix: ''},// "legs",
-    // total: 1000,
-};
-
-
 
 function DisplayCurrentSelection({ ...props }) {
 
@@ -52,33 +19,18 @@ function DisplayCurrentSelection({ ...props }) {
 
   const selections = data.orders 
   
-  console.log(selections)
-//  console.log(LanguageTranslater['doors']['heb']); 
-
+  
+  // console.log(selections)
  let currentStep = context.fakeData.order.stageNo
   let ListTitle = selections.furniture//"הארון שלי"
 
-  const keys = Object.keys(selections)
-  console.log(keys);
+  // const keys = Object.keys(selections)
+  // console.log(keys);
   return (
-    <div className="currentSelection">
+    <div className={styles.currentSelection}>
 
-      <h3 className='ListTitle'>{ListTitle}</h3>
-
-      <ul className='Selectionlist'>
-        {/* TODO: add sort() after filter */}
-        {keys.filter((v)=>LanguageTranslater[v]).sort((a,b)=>LanguageTranslater[a]['ordinal']<LanguageTranslater[b]['ordinal']).map((k) => 
-        <li>{
-          typeof selections[k] === 'object' ? k : 
-          <SelectionRow bolder= {LanguageTranslater[k]['heb']} regular={selections[k] + ' ' + LanguageTranslater[k]['postfix']}/>
-          // <div className='SelectionRow'>
-          
-
-          // </div> 
-          }
-        </li>)}
-
-      </ul>
+      <h3 className={styles.ListTitle}>{ListTitle}</h3>
+      <CurrentSelections selections={selections}/>
 
     </div>
   )
