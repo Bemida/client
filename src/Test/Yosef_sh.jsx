43ad9b8017853e28a094bd92de518f840d417752
yosefschmidt <yosef74526@gmail.com>
2023-06-28 14:16:12 +0300

    add chenge

--- src/Test/Yosef_sh.jsx
+++ src/Test/Yosef_sh.jsx
@@ -3,15 +3,19 @@ import Button from '../Components/Button'
 // import Payment from '../Components/Payment'
 import axios from 'axios';
 import orders from '../Data/fakeDataTest'
+import { messageTemplate } from '../Data/messageTemplate';
 
 
 export default function Yosef_sh(props) {
-
-async function createHtmlEmail(){
-  return {"email":orders.orders.userEmail,"title":"work!!!","html":"<h1>555 <h2>wertyuioiuytr</h2></h1>"}
-}
-
-async function hendleSubmit(){
+  async function createHtmlEmail(){
+    const data =messageTemplate(orders.orders)
+    console.log(orders.orders.userEmail)
+    return data
+    // return {"email":orders.orders.userEmail,"title":"work!!!","html":"<h1>555 <h2>wertyuioiuytr</h2></h1>"}
+  }
+  
+  async function hendleSubmit(){
+  // if(1==1){throw "good"}
   const data= await createHtmlEmail()
   // const data={"email":orders.orders.userEmail,"title":"work!!!","html":"<h1>555 <h2>wertyuioiuytr</h2></h1>"}
   axios.post('http://localhost:2023/api/others/sendemail/', data)
