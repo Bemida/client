8f8d64c2eb0c3b82a7f6f76c933841f68780e5e9
j s <grnwtmry@gmail.com>
2023-06-29 19:46:58 +0300

    test

--- src/Layout/DisplayCurrentSelection/index.jsx
+++ src/Layout/DisplayCurrentSelection/index.jsx
@@ -1,8 +1,7 @@
 import { interpolate } from '@react-spring/core'
 import styles from './style.module.css'
-
 import { DataContext } from '../../Context/MainContext'
-import { useContext } from 'react'
+import { useContext, } from 'react'
 
 
 import data from '../../Data/fakeDataTest' //.orders
@@ -18,18 +17,6 @@ function DisplayCurrentSelection({ ...props }) {
   const context = useContext(DataContext)
 
   const selections = context.order//data.orders 
-   /*yonatan*/ 
-   const [finalPayment, setFinalPayment] = useState()
-  useEffect(() => {
-    // if the page is 5?
-    axios.post("")
-    .then((res) => {
-      setFinalPayment(res.data)
-      console.log(res.data)
-    })
-      .catch(err => console.log(err))
-  },[])
-   /*yonatn*/
   
    console.log(selections)
  let currentStep = context.fakeData.order.stageNo
