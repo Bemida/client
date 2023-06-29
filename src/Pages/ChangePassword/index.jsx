3d22edf031fde874ecefc022a2672b8ac479ae60
David <computerdavid494@gmail.com>
2023-06-29 13:25:24 +0300

    Add user changePassword page

--- src/Pages/ChangePassword/index.jsx
+++ src/Pages/ChangePassword/index.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import styles from './style.module.css'
+import { useState } from 'react'
+import api from '../../Functions/API_Calls/apiCalls'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function ChangePassword() {
+    const [password, setPassword] = useState("")
+    const [passwordVerification, setPasswordVerification] = useState("")
+    const [isEqual, setIsEqual] = useState(false);
+    const navigate = useNavigate()
+
+    const handelPasswordChange = (e) => {
+        if (e.target.name === "password") {
+            setPassword(e.target.value);
+        }
+        else {
+            setPasswordVerification(e.target.value);
+        }
+    }
+
+    const handelSubmit = (e) => {
+        if (password === passwordVerification && api.put(password, passwordVerification, localStorage.token) !== "Password changed successfully") {
+            setIsEqual(true);
+        }
+        else {
+            navigate("/")
+        }
+    }
+
     return (
-        <div>ChangePassword</div>
+        <div className={styles.warp}>
+            <p>.איפוס סיסמא</p>
+            <p>!קורה לכולנו </p>
+            <form method="post">
+                <label htmlFor='password'>סיסמא חדשה</label>
+                <input type="password" value={password} onInput={handelPasswordChange} name='password' />
+                <label htmlFor='passwordVerification'>אימות סיסמא </label>
+                <input type="password" value={passwordVerification} onInput={handelPasswordChange} name='passwordVerification' />
+                <button type="button" onClick={handelSubmit}>המשך</button>
+                {isEqual && "הסיסמא אינה זהה"}
+            </form>
+        </div>
     )
 }
---
+++ src/Pages/Register/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './style.module.css'
+
+export default function Register() {
+    return (
+        <div>Register</div>
+    )
+}
