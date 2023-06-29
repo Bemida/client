d0dc0f775a6acadc382f05b4e1e2e439b1afafb0
David <computerdavid494@gmail.com>
2023-06-29 11:23:13 +0300

    start working on user pages

--- src/Routes/mainRoutes.jsx
+++ src/Routes/mainRoutes.jsx
@@ -2,11 +2,17 @@ import { Route, Routes } from "react-router-dom";
 
 import React from "react";
 import Layout from "../Layout";
+import ChangePassword from "../Pages/ChangePassword";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 function MainRoutes() {
   return (
     <Routes>
       <Route index element={<Layout />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/changepassword" element={<ChangePassword />} />
     </Routes>
   );
 }
